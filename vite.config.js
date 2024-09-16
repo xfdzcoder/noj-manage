76196d0fdc44d1a1dae186eba6b5216a7ebce768
xfdzcoder <xfdzcoder@gmail.com>
2024-09-16 13:34:43 +0800

    fix: 修改运行端口

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,9 @@ const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5174
+  },
   plugins: [
     VueRouter({
       extensions: ['.vue'],
